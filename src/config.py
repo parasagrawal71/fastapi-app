43c69072c07d1a3190039ce63e356d1054048ec7
Paras Agrawal <parasagrawal71@gmail.com>
2023-07-16 19:10:29 +0530

    Read config from env file

--- src/config.py
+++ src/config.py
@@ -3,6 +3,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     APP_NAME: str = "FastAPI Server"
+    MONGODB_URI: str = ""  # Mandatory in order to make .env work
+    DATABASE_NAME: str = "fastapi-app"
+    MAX_CONNECTIONS_COUNT: int = 10
+    MIN_CONNECTIONS_COUNT: int = 10
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 config = Settings()
