8927e61f5b838203e2236823448f7cbfad678192
Paras Agrawal <parasagrawal71@gmail.com>
2023-07-16 15:13:18 +0530

    Add CORS middleware and startup & shutdown  events

--- src/main.py
+++ src/main.py
@@ -1,13 +1,34 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.api import items
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["DELETE", "GET", "POST", "PUT"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def healthcheck():
     return "Ok"
+
+
+@app.on_event("startup")
+def startup():
+    print("Starting fastapi server...")
+
+
+@app.on_event("shutdown")
+def shutdown():
+    print("Closing fastapi server...")
 
 
 app.include_router(items.router)
